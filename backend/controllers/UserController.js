b87bfa871965bbda35735bbb991d6c85661ded1b
lakdiludilshan <lakdiludilshan@gmail.com>
2024-06-18 09:02:41 +0530

    login function implemented

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,6 +1,7 @@
-const asyncHandler = require('express-async-handler')
-const User = require('../models/UserModel')
-const jwt = require('jsonwebtoken')
+const asyncHandler = require('express-async-handler');
+const User = require('../models/UserModel');
+const jwt = require('jsonwebtoken');
+const bycrypt = require('bcryptjs');
 
 const generateToken = (id) => {
     return jwt.sign({id}, process.env.JWT_SECRET, {
@@ -62,7 +63,51 @@ const registerUser = asyncHandler( async (req, res) => {
 //Login user
 
 const loginUser = asyncHandler( async (req, res) => {
-    res.send("Login user");
+    
+    const { email, password } = req.body;
+
+    console.log(email, password)
+
+    if(!email || !password) {
+        res.status(400);
+        throw new Error("Please fill all requierd fields")
+    }
+
+    //check if the user exists
+    const user = await User.findOne({ email })
+
+    console.log(user)
+
+    if(!user) {
+        res.status(400);
+        throw new Error("Invalid Email")
+    }
+
+    //check if the password is correct
+    const isPasswordMatch = await bycrypt.compare(password, user.password);
+
+     //Genenrate token
+     const token = generateToken(user._id);
+
+     //Send HTTP only cookie
+     res.cookie("token", token, {
+         path: "/",
+         httpOnly: true,
+         expires: new Date(Date.now() +1000 * 86400), // 1 day
+         sameSite: "none",
+         secure: true,
+     });
+
+    if (user && isPasswordMatch) {
+        const { _id, name, email, photo, phone } = user
+        res.status(201).json({
+            _id, name, email, photo, phone, token,
+        });
+
+    } else {
+        res.status(400);
+        throw new Error("Invalid user data")
+    }
 });
 
 module.exports = {
--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -1,60 +1,64 @@
-const mongoose = require('mongoose');
-const bycrypt = require('bcryptjs');
+const mongoose = require("mongoose");
+const bycrypt = require("bcryptjs");
 
-const UserSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: [true, "Please provide a name"],
-        min: 6,
-        max: 255
+      type: String,
+      required: [true, "Please provide a name"],
+      min: 6,
+      max: 255,
     },
     email: {
-        type: String,
-        required: [true, "Please provide an email"],
-        unique: true,
-        trim: true,
-        match: [/.+\@.+\..+/, "Please provide a valid email address"],
-        min: 6,
-        max: 255
+      type: String,
+      required: [true, "Please provide an email"],
+      unique: true,
+      trim: true,
+      match: [/.+\@.+\..+/, "Please provide a valid email address"],
+      min: 6,
+      max: 255,
     },
     password: {
-        type: String,
-        required: [true, "Please provide a password"],
-        min: [6, "Password must be at least 6 characters long"],
-        max: 125
+      type: String,
+      required: [true, "Please provide a password"],
+      min: [6, "Password must be at least 6 characters long"],
+      max: 125,
     },
     phone: {
-        type: String,
-        required: [true, "Please provide a phone number"],
-        default: "+94",
-        min: [10, "Phone number must be at least 10 characters long"],
-        max: 13
+      type: String,
+      required: [true, "Please provide a phone number"],
+      default: "+94",
+      min: [10, "Phone number must be at least 10 characters long"],
+      max: 13,
     },
     username: {
-        type: String,
+      type: String,
       //  required: [true, "Please provide a username"],
-        min: 6,
-        max: 255
+      min: 6,
+      max: 255,
     },
     photo: {
-        type: String,
-        required: [true, "Please provide a photo"],
-        default: "empty-profile.png"
+      type: String,
+      required: [true, "Please provide a photo"],
+      default: "empty-profile.png",
     },
-}, { timestamps: true, });
+  },
+  { timestamps: true }
+);
 
-    // hash the password
-    UserSchema.pre("save", async function (next) {
-     if(this.isModified("password")) {
-        return next();
-     }
+// hash the password
+UserSchema.pre("save", async function (next) {
+  // hash the password only if it has been modified (or is new)
+  if (!this.isModified("password")) {
+    next();
+  }
 
-        //hashedpassword
-        const salt = await bycrype.genSalt(10)
-        const hashedPassword = await bycrype.hash(this.password, salt);
-        this.password = hashedPassword;
-        next();
-    })
+  //hashedpassword
+  const salt = await bycrypt.genSalt(10);
+  const hashedPassword = await bycrypt.hash(this.password, salt);
+  this.password = hashedPassword;
+  next();
+});
 
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.model("User", UserSchema);
 module.exports = User;
