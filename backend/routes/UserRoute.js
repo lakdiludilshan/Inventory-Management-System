0488bdac2107f688f65c86136addd21b110629ea
lakdiludilshan <lakdiludilshan@gmail.com>
2024-06-18 09:59:33 +0530

    logout functionality implemented

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { registerUser, loginUser} = require('../controllers/UserController');
+const { registerUser, loginUser, logout} = require('../controllers/UserController');
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/logout", logout);
 
 
 module.exports = router;
