6db30980441a3d7e6c0c84d9337b37b9cfd201c4
lakdiludilshan <lakdiludilshan@gmail.com>
2024-07-23 13:59:54 +0530

    cart initiated

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,9 +1,11 @@
 import {configureStore} from "@reduxjs/toolkit";
 import { authReducer } from "../features/authentication/slice/authSlice";
+import cartSlice from "../features/cart/slices/cartSlice";
 
 const store = configureStore({
     reducer: {
         auth: authReducer,
+        cart: cartSlice.reducer,
     },
 })
 
